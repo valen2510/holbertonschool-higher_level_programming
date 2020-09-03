ce062b10e9a6e070a42914e2afa72cd91c125cce
valen2510 <valenjaramilo1025@hotmail.com>
2020-09-03 20:44:51 +0000

    Fix message in more than 1 argument and add condition when 2 arguments are entered

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,9 +3,11 @@ if __name__ == "__main__":
     import sys
     argv_len = len(sys.argv)
     if argv_len == 1:
-            print(" {:d} arguments.".format(argv_len - 1))
-    else:
+        print(" {:d} arguments.".format(argv_len - 1))
+    elif argv_len == 2:
         print("{:d} argument:".format(argv_len - 1))
+    else:
+        print("{:d} arguments:".format(argv_len - 1))
         for i in range(argv_len):
             if i != 0:
                 print("{:d}: {:s}".format(i, sys.argv[i]))
