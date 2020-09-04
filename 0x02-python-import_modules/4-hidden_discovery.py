0165bd4ef0be2707468eb00c8487a26ab3749137
valen2510 <valenjaramilo1025@hotmail.com>
2020-09-04 03:26:54 +0000

    Add import requirement

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-import hidden_4
-for i in dir(hidden_4):
-    if i[0] != "_":
-        print("{}".format(i))
+if __name__ == "__main__":
+    import hidden_4
+    for i in dir(hidden_4):
+        if i[0] != "_":
+            print("{}".format(i))
