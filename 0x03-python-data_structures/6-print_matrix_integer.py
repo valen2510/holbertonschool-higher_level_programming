bd05614d57e43da84b7d19a1d02a019bceb45fb4
valen2510 <valenjaramillo1025@hotmail.com>
2020-09-08 22:00:22 +0000

    Fix new lines

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix:
         for rows in matrix:
             for columns in rows:
-                print("{:d}".format(columns), end=" ")
+                print("{:d}".format(columns), end="")
+                if columns != rows[-1]:
+                    print(" ", end="")
             print()
