f68235b07ae757911914beace0e3a737c3a8a9a2
valen2510 <valenjaramillo1025@hotmail.com>
2020-09-09 23:00:10 +0000

    Function to print info about Python lists

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,16 @@
+#include <Python.h>
+#include <stdio.h>
+/**
+ * print_pyhton_list-info - prints info from a pyhton list using Cpyhton
+ * @p: PyObject of the list
+ **/
+void print_python_list_info(PyObject *p)
+{
+	int counter;
+
+	printf("[*] Size of the Python List = %ld\n", PyList_Size(p));
+	printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
+
+	for (counter = 0; counter < PyList_Size(p); counter++)
+		printf("Element %d = %s\n", counter, Py_TYPE(PyList_GetItem(p, counter))->tp_name);
+}
