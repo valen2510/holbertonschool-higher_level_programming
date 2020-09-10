eb4fc951e54317a5b06d0bdadc4d548c250e7a3b
valen2510 <valenjaramillo1025@hotmail.com>
2020-09-10 16:05:12 +0000

    List Comprehension

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = my_list.copy()
-    for i in range(len(new_list)):
-        if new_list[i] == search:
-            new_list[i] = replace
-    return new_list
+    return [replace if i == search else i for i in my_list]
