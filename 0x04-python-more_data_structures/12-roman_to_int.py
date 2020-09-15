4c2fd9524cdb9694d9408155da8f94564c1713b3
valen2510 <valenjaramillo1025@hotmail.com>
2020-09-15 01:45:54 +0000

    Fix sintaxys

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if type(roman_string) != srt or roman_string is None:
+    if type(roman_string) != str or roman_string is None:
         return 0
 
     dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
