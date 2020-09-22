2d144deb559a18a674f440123d2271ae19ca5785
valen2510 <valenjaramillo1025@hotmail.com>
2020-09-22 02:21:16 +0000

    Use *args in fct

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -2,7 +2,7 @@
 def safe_function(fct, *args):
     from sys import stderr
     try:
-        return fct(args[0], args[1])
+        return fct(*args)
     except Exception as ex:
         print("Exception: {}".format(ex), file=stderr)
         return None
