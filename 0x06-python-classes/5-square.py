7dacda61404a41d7c420738d1fda3c7c17674a4f
valen2510 <valenjaramillo1025@hotmail.com>
2020-09-23 04:19:58 +0000

    class Square that defines a square  that validates size value, attribute a  Public instance method, get and set a value for a private instance attribute and print square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""Square class"""
+
+
+class Square:
+    """Define Square class"""
+    def __init__(self, size=0):
+        """Validate size value and initialize Square attribute"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Define Square area"""
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """Get size value"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set size value"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def my_print(self):
+        """Print the Square with #"""
+        if self.__size == 0:
+            print("")
+        else:
+            for i in range(self.__size):
+                print("#" * self.__size)
