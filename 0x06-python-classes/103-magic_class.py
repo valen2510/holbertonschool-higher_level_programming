132f33b6fd44c0c507aa093f7c46802aba271af9
valen2510 <valenjaramillo1025@hotmail.com>
2020-09-24 00:49:47 +0000

    import math module and fix attribute

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
 """MagicClass class"""
+import math
 
 
 class MagicClass:
-    """Define Magicclass"""
+    """Initialize and validate attributes"""
 
     def __init__(self, radius):
-        """Initialize and validate attributes"""
-        self._MagicClass__radius = 0
+        self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError('radius must be a number')
-        self._MagicClass__radius = radius
+        self.__radius = radius
 
     def area(self):
         """Define method area"""
-        return self._MagicClass__radius ** 2 * math.pi
+        return self.__radius ** 2 * math.pi
 
     def circumference(self):
         """define method circumference"""
-        return 2 * math.pi * self._MagicClass__radius
+        return 2 * math.pi * self.__radius
