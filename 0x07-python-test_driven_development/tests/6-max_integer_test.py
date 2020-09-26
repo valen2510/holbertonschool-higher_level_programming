15354850131e13f6570c26bbc9441864551ede27
valen2510 <valenjaramillo1025@hotmail.com>
2020-09-26 07:02:13 +0000

    Add tests for max number at the beggining and the only one

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -7,10 +7,11 @@ max_integer = __import__('6-max_integer').max_integer
 class TestMaxInteger(unittest.TestCase):
     def test_maxint(self):
         """Test different number lists"""
-        self.assertEqual(max_integer([6, 9, 15, 6]), 15)
+        self.assertEqual(max_integer([15, 9, 6]), 15)
         self.assertEqual(max_integer([10, 10, 10]), 10)
         self.assertEqual(max_integer([-54, -7, 0, -84, -25]), 0)
         self.assertEqual(max_integer([4.8, 3.2, 9.2]), 9.2)
+        self.assertEqual(max_integer([38]), 38)
         
     def test_emptylist(self):
         """Test void list"""
