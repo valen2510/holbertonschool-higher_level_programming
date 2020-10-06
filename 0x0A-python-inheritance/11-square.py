5ccee7ce32c3836ef5e99b723e83d9935e655674
valen2510 <valenjaramillo1025@hotmail.com>
2020-10-06 20:47:17 +0000

    Square class with sttr method added

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Square class
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Define Square class
+
+    Args:
+        Rectangle (BaseGeometry): subclass of BaseGeometry
+    """
+    def __init__(self, size):
+        """Initialisation
+
+        Args:
+            size (int): positive integer
+        """
+        super().__init__(size, size)
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def __str__(self):
+        """Return a description of Square
+
+        Returns:
+            str: [Square] <width>/<height>
+        """
+        return "[Square] {}/{}".format(self.__size, self.__size)
