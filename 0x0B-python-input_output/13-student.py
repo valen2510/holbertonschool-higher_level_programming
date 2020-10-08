e9e32362b35c65d1801b5187065448ee9915477a
valen2510 <valenjaramillo1025@hotmail.com>
2020-10-08 04:46:32 +0000

    Replace all attributes of the student instance

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Class Student
+"""
+
+
+class Student:
+    """Define class Student
+    """
+    def __init__(self, first_name, last_name, age):
+        """Initialisation of public instance attributes
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Public method to retrieve a dictionary representation
+            of a student instance
+        """
+        if attrs is not None:
+            dict_student = {}
+            for k in self.__dict__:
+                if k in attrs:
+                    dict_student[k] = self.__dict__[k]
+            return dict_student
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of the Student instance
+
+        Args:
+            json (dict): instance dictionary 
+        """
+        if json:
+            self.__dict__ = json
