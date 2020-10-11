fdf8cd6b716ccebfed93e1aba974b68d1a6244e0
valen2510 <valenjaramillo1025@hotmail.com>
2020-10-11 05:55:20 +0000

    Defining Base Class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,96 @@
+#!/usr/bin/python3
+"""Define Base class
+"""
+
+
+import json
+
+
+class Base:
+    """Define Base class
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initialisation
+
+        Args:
+            id (int, None): integer identification. Defaults to None.
+        """
+        if id:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """ Function to returns the JSON string
+        representation of list_dictionaries
+
+        Args:
+            list_dictionaries (dict): instance dictionary
+
+        Returns:
+            str: JSON object as a string
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return []
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """writes the JSON string representation
+        of list_objs to a file
+
+        Args:
+            list_objs (objects): class instances
+        """
+        list_cls = []
+        if list_objs is not None:
+            for i in list_objs:
+                list_cls += [(cls.to_dictionary(i))]
+
+        with open(cls.__name__ + ".json", "w", encoding="utf-8") as writer:
+            writer.write(cls.to_json_string(list_cls))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Function that returns the list of
+        the JSON string representation
+
+        Args:
+            json_string (str): JSON string representation
+
+        Returns:
+            list : list of JSON string
+        """
+        if json_string is not None or json_string != "":
+            return json.loads(json_string)
+        return []
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Function with instance
+        with all attributes already set
+
+        Returns:
+            [object]: instance of the class
+        """
+        dummy = cls(1, 1)
+        dummy.update(**dictionary)
+        return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        """Function that load the list of JSON strings,
+        create instances and returns a list of instances
+
+        Returns:
+            list : list of instances
+        """
+        if not cls.__name__ = ".json":
+            return []
+        with open(cls.__name__ + '.json', encoding="utf-8") as reader:
+            data = cls.from_json_string(reader.read())
+        return [cls.create(**dic) for dic in data]
