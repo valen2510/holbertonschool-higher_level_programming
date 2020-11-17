ac5b90b9094e997764d1cb118cebea8dc59bdf94
valen2510 <valenjaramillo1025@hotmail.com>
2020-11-17 16:56:18 -0500

    | 01 | Script that creates the table id_not_null on MySQL server.|

---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,3 @@
+-- Script that creates the table id_not_null on MySQL server.
+-- id_not_null description: id INT with the default value 1 and name VARCHAR(256).
+CREATE TABLE IF NOT EXISTS id_not_null (id INT NOT NULL DEFAULT 1, name VARCHAR(256));
