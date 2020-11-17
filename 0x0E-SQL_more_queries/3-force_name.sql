cc9edd9d6f6a3db3c8b054b1351b94f645602460
valen2510 <valenjaramillo1025@hotmail.com>
2020-11-17 16:44:28 -0500

    | 01 | Script that creates the table force_name on MySQL server.|

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,3 @@
+-- Script that creates the table force_name on MySQL server.
+-- force_name description: id INT and name VARCHAR(256).
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
