b26d88f6b9879ed19872d2db67a475bc8beca0f6
valen2510 <valenjaramillo1025@hotmail.com>
2020-11-17 21:19:43 -0500

    | 01 | Script that lists all shows contained in the database hbtn_0d_tvshows.|

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,3 @@
+-- Script that lists all shows contained in the database hbtn_0d_tvshows.
+-- Each record should display: tv_shows.title - tv_show_genres.genre_id.
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_show_genres RIGHT JOIN tv_shows ON tv_show_genres.show_id=tv_shows.id ORDER BY tv_shows.title, tv_show_genres.genre_id;
