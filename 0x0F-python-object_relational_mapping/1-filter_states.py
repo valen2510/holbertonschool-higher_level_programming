7d5e9cd8d3e19a47f7606f54d01c2d3350ad9520
valen2510 <valenjaramillo1025@hotmail.com>
2020-12-12 10:23:08 -0500

    | 01 | Lists all the states with a name starting with N. |

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Lists all the states with a name starting with N
+    from database hbtn_0e_0_usa.
+"""
+
+
+import MySQLdb
+from sys import argv
+
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+    row = cur.fetchall()
+    for state in row:
+        print(state)
+    cur.close()
+    db.close()
