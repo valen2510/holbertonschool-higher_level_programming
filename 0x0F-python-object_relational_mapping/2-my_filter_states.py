b29d8f1f3d929ace6c5b2324a811e431e78cf941
valen2510 <valenjaramillo1025@hotmail.com>
2020-12-12 10:24:29 -0500

    | 01 | Display all the values in the states that matches an argument. |

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Display all the values in the states table where matches the argument
+"""
+
+
+import MySQLdb
+from sys import argv
+
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE '%{}%'".format(argv[4]))
+    row = cur.fetchall()
+    for state in row:
+        print(state)
+    cur.close()
+    db.close()
