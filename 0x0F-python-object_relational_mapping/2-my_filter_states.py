33732cea247fcb537ac4596257fc2e27341d7553
valen2510 <valenjaramillo1025@hotmail.com>
2020-12-12 12:24:18 -0500

    | 04 | Fix pep8. |

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -11,7 +11,8 @@ if __name__ == '__main__':
     db = MySQLdb.connect(host='localhost', port=3306,
                          user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id".format(str(argv[4])))
+    cur.execute("SELECT * FROM states WHERE name = '{}' \
+                ORDER BY id".format(str(argv[4])))
     row = cur.fetchall()
     for state in row:
         print(state)
