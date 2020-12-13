2ba235a3121d838d5598b5333a7b507d1e213823
valen2510 <valenjaramillo1025@hotmail.com>
2020-12-13 18:44:39 -0500

    | 02 | Add BINARY to LIKE query. |

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -9,14 +9,14 @@ from sys import argv
 if __name__ == '__main__':
     db = MySQLdb.connect(host='localhost', port=3306,
                          user=argv[1], passwd=argv[2], db=argv[3])
-    cur = db.cursor()
-    cur.execute("SELECT cities.name FROM cities RIGHT JOIN states ON \
-                states.id=cities.state_id WHERE states.name = %s", (argv[4], ))
-    row = cur.fetchall()
+    cu = db.cursor()
+    cu.execute("SELECT cities.name FROM cities RIGHT JOIN states ON states.id=\
+                cities.state_id WHERE states.name LIKE BINARY %s", (argv[4], ))
+    row = cu.fetchall()
     for city in row:
         if city is row[-1]:
             print(city[0])
         else:
             print(city[0], end=", ")
-    cur.close()
+    cu.close()
     db.close()
