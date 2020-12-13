c4c7a4a0ed7f2175afc875a6596368f424e1b8d9
valen2510 <valenjaramillo1025@hotmail.com>
2020-12-13 18:50:25 -0500

    | 03 | Add ORDER BY AND change RIGHT for INNER in query. |

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -10,8 +10,9 @@ if __name__ == '__main__':
     db = MySQLdb.connect(host='localhost', port=3306,
                          user=argv[1], passwd=argv[2], db=argv[3])
     cu = db.cursor()
-    cu.execute("SELECT cities.name FROM cities RIGHT JOIN states ON states.id=\
-                cities.state_id WHERE states.name LIKE BINARY %s", (argv[4], ))
+    cu.execute("SELECT cities.name FROM cities INNER JOIN states ON states.id=\
+                cities.state_id WHERE states.name LIKE BINARY %s \
+                    ORDER BY cities.id", (argv[4], ))
     row = cu.fetchall()
     for city in row:
         if city is row[-1]:
