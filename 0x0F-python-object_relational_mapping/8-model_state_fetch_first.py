30e5329ab3c3e403edaaa233642f69fa3bcfd0b2
valen2510 <valenjaramillo1025@hotmail.com>
2020-12-13 19:16:08 -0500

    | 02 | Add condition to print first element or Nothing. |

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -2,7 +2,7 @@
 """ Prints the first State object from the database hbtn_0e_6_usa
 """
 
-from model_state import State
+from model_state import State, Base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
 from sys import argv
@@ -12,5 +12,8 @@ if __name__ == '__main__':
         'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]))
     session = Session(bind=engine)
     first_row = session.query(State).first()
-    print('{}: {}'.format(first_row.id, first_row.name))
+    if first_row:
+        print('{}: {}'.format(first_row.id, first_row.name))
+    else:
+        print("Nothing")
     session.close()
