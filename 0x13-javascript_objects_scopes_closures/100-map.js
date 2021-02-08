423fc2e270627753ec2a8bf2842fb5a3261f5037
valen2510 <valenjaramillo1025@hotmail.com>
2021-02-08 13:31:38 -0500

    | 02 | Fix standard style. |

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -2,6 +2,6 @@
 
 const arrayData = require('./100-data').list;
 
-const arrayDataIndex = arrayData.map(function(currentValue, index) {return currentValue * index});
+const arrayDataIndex = arrayData.map(function (currentValue, index) { return currentValue * index; });
 console.log(arrayData);
 console.log(arrayDataIndex);
