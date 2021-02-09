b05d84b6b8a7f53f5844e5b183da0b9da1cf2d17
valen2510 <valenjaramillo1025@hotmail.com>
2021-02-09 13:18:05 -0500

    | 03 | Scope the counter into else. |

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -5,10 +5,10 @@ const url = process.argv[2];
 const wedge = 'https://swapi-api.hbtn.io/api/people/18/';
 
 request(url, function (error, response, body) {
-  let counter = 0;
   if (error) {
     console.log(error);
   } else {
+    let counter = 0;
     for (const i of JSON.parse(body).results) {
       if (i.characters.includes(wedge)) {
         counter++;
