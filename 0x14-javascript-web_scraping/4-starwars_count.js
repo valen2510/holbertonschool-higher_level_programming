a6b81da3cf0b2384c0674d65498f137eca861340
valen2510 <valenjaramillo1025@hotmail.com>
2021-02-09 14:08:53 -0500

    | 05 | Change include for iteration and slice. |

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,16 +2,17 @@
 
 const request = require('request');
 const url = process.argv[2];
-const wedge = 'https://swapi-api.hbtn.io/api/people/18/';
 
 request(url, function (error, response, body) {
   if (error) {
-    console.log(error);
+    console.error(error);
   } else if (JSON.parse(body).results) {
     let counter = 0;
-    for (const i of JSON.parse(body).results) {
-      if (i.characters.includes(wedge)) {
-        counter++;
+    for (const movies of JSON.parse(body).results) {
+      for (const character of movies.characters) {
+        if (character.slice(-3, -1) === '18') {
+          counter++;
+        }
       }
     }
     console.log(counter);
