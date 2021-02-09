6e7f91c41bb40048ada3cfa6b845d451a7a9cee9
valen2510 <valenjaramillo1025@hotmail.com>
2021-02-09 13:22:34 -0500

    | 04 | Add condition to parse. |

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -7,7 +7,7 @@ const wedge = 'https://swapi-api.hbtn.io/api/people/18/';
 request(url, function (error, response, body) {
   if (error) {
     console.log(error);
-  } else {
+  } else if (JSON.parse(body).results) {
     let counter = 0;
     for (const i of JSON.parse(body).results) {
       if (i.characters.includes(wedge)) {
