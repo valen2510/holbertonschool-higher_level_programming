712f84f827a8c4638283d1133b8454534c5c3329
valen2510 <valenjaramillo1025@hotmail.com>
2021-02-09 13:14:34 -0500

    | 01 | Get the contents of a webpage and stores it in a file. |

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+
+const url = process.argv[2];
+const file = process.argv[3];
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    fs.writeFile(file, body, 'utf-8', function (err) {
+      if (err) { console.log(err); }
+    });
+  }
+});
